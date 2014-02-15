f2dbef4677a54a3ef700a5948c2ad27191551462
Arsen Bagratyan <arsenbagratyan01@gmail.com>
2014-02-15 21:17:19 +0400

    Sidebar

---
+++ application/views/admin/_sidebar.php
@@ -0,0 +1,10 @@
+<div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">
+    <div class="list-group">
+        <a href="#" class="list-group-item">Course manager</a>
+        <a href="#" class="list-group-item">All Course Statistics</a>
+        <a href="#" class="list-group-item active">Student Manager</a>
+        <a href="#" class="list-group-item">All Student Statistics</a>
+        <a href="#" class="list-group-item">All Finance View</a>
+        <a href="#" class="list-group-item">Product Manager</a>
+    </div>
+</div><!--/span-->
